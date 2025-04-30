ba03b1fd2573fcc13cd559f9d5fb8d23ae14e8f7
Sumair Shaikh <shaikhsumair01@gmail.com>
2025-04-30 18:23:15 +0530

    Added highscore to the project

--- Scoreboard.py
+++ Scoreboard.py
@@ -5,7 +5,9 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score= 0
-        self.highscore = 0
+        with open("data.txt", mode = "r") as file:
+            previous_score = file.read()
+        self.highscore = int(previous_score)
         self.color("white")
         self.penup()
         self.goto(0,270)
@@ -19,6 +21,8 @@ class Scoreboard(Turtle):
     def reset_score(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open("data.txt", mode = "w") as file:
+                file.write(str(self.highscore))
         self.score = 0
         self.update_score()
 
